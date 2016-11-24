c6caf7a0a5b466d40484e790da097f84463a3f76
Vitor Hugo Fernandes Lopes <vitor.automec@hotmail.com>
2016-11-23 22:51:02 -0200

    Finalizando aula 2 de AngularJS

---
+++ front-end/frameworks-mvc/angularjs/alurapic/public/js/controllers/PicturesCtrl.js
@@ -0,0 +1,12 @@
+angular.module('alurapic')
+
+    .controller('PicturesCtrl', function ($scope) {
+
+        $scope.picture = {
+            url: 'http://vignette1.wikia.nocookie.net/leagueoflegends/images/1/12/League_of_Legends_Icon.png/revision/latest?cb=20150402234343',
+            title: 'lol-logo'
+        };
+
+    })
+
+;
