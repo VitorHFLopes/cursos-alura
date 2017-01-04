92e09422b1b89cd7689293419e30033230ec7160
Vitor H. Lopes <vitor.automec@hotmail.com>
2017-01-03 23:08:21 -0200

    Finalizando aula 5 Ionic Framework

--- mobile/multiplataforma/ionic/AluraCar/www/js/controller.js
+++ mobile/multiplataforma/ionic/AluraCar/www/js/controller.js
@@ -3,16 +3,16 @@ angular.module('starter')
     .controller('ListCtrl', function ($scope) {
 
         $scope.carsList = [
-            {name: 'BMW 120i', price: '100000'},
-            {name: 'ONIX 1.0', price: '20000'},
-            {name: 'FIESTA 1.0', price: '30000'},
-            {name: 'C3 1.0', price: '80000'},
-            {name: 'UNO FIRE', price: '10000'},
-            {name: 'SENTRA 2.0', price: '40000'},
-            {name: 'ASTRA SEDAN', price: '20000'},
-            {name: 'VECTRA 2.0 TURBO', price: '30000'},
-            {name: 'HILUX 4X4', price: '200000'},
-            {name: 'MONTANA', price: '30000'}
+            {name: 'BMW 120i', price: 100000},
+            {name: 'ONIX 1.0', price: 20000},
+            {name: 'FIESTA 1.0', price: 30000},
+            {name: 'C3 1.0', price: 80000},
+            {name: 'UNO FIRE', price: 10000},
+            {name: 'SENTRA 2.0', price: 40000},
+            {name: 'ASTRA SEDAN', price: 20000},
+            {name: 'VECTRA 2.0 TURBO', price: 30000},
+            {name: 'HILUX 4X4', price: 200000},
+            {name: 'MONTANA', price: 30000}
         ];
 
     })
@@ -20,6 +20,22 @@ angular.module('starter')
     .controller('ChosenCarCtrl', function ($scope, $stateParams) {
 
         $scope.car = angular.fromJson($stateParams.car);
+
+        $scope.accessoryList = [
+            {name: 'Freio ABS', price: 800},
+            {name: 'Ar Condicionado', price: 1000},
+            {name: 'MP3 Player', price: 500}
+        ];
+
+        $scope.change = function (accessory, isChecked) {
+
+            if(isChecked) {
+                $scope.car.price += accessory.price;
+            } else {
+                $scope.car.price -= accessory.price;
+            }
+
+        };
 
     })
 
