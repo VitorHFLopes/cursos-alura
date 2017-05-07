02d174f2b5792e3597b4ac07dc479f5456d12472
Vitor Hugo F Lopes <vitor.automec@hotmail.com>
2017-05-07 17:52:58 -0300

    Iniciando consumo do banco con NodeJS

--- programacao/node/sitecasadocodigo/app/routes/produtos.js
+++ programacao/node/sitecasadocodigo/app/routes/produtos.js
@@ -1,10 +1,38 @@
+var mysql = require('mysql');
+
+var connectionProperties = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'casadocodigo'
+};
+
+var connection = mysql.createConnection(connectionProperties);
+
 function rotasProdutos(app) {
 
     app.get('/produtos', getProdutos);
 
     function getProdutos(request, response) {
 
-        response.render('produtos/lista');
+        connection.query('select * from produtos', getProdutos);
+
+        //primeiro parametro de uma resposta do banco é sempre um erro
+        function getProdutos(error, produtos) {
+
+            if (error) {
+
+                console.log(error);
+            }
+
+            var viewParams = {
+                lista: produtos
+            };
+
+            response.render('produtos/lista', viewParams);
+        }
+
+        connection.end();
     }
 }
 
