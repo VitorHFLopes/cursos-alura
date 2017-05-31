3a7b02b2b85ca455583ad75f2dcf2f91d5b96a27
Vitor Hugo F Lopes <vitor.automec@hotmail.com>
2017-05-30 23:00:53 -0300

    Iniciando curso de programação em C

---
+++ programacao/c/adivinhacao/adivinhacao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
+
+    int numeroSecreto = 42;
+
+    int chute;
+
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
+}
+
+/*
+	%d -> adiciona uma variavel INT na string
+*/
